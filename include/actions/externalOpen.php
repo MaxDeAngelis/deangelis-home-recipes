9bddfca81edbcb4de19b18a5319538005ef90a96
Maxwell DeAngelis <max@Maxwells-MacBook-Pro.local>
2018-01-11 20:48:16 -0500

    Added unirest and updates to use spoonacular API and left others commented out.

--- include/actions/externalOpen.php
+++ include/actions/externalOpen.php
@@ -10,11 +10,23 @@ class ExternalOpen extends Action {
 	}
 
 	public function process() {
-		$endpoint = "https://food2fork.com/api/get?";
-		$key = "key=1132f2389852e19d629e0b7d9d7b784f";
-		$rId = "rId=" . $this->id;
+		$use = 1;
 
-		$response = file_get_contents($endpoint . $key . "&" . $rId);
+		if ($use == 1) {
+			$response = Unirest\Request::get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" .  $this->id. "/information",
+				array(
+					"X-Mashape-Key" => "szCweNvVtTmshzgBOVW0iilv2Brtp1XPFwijsnTw76PcD3MKwf",
+					"Accept" => "application/json"
+				)
+			);
+		} else {
+			$endpoint = "https://food2fork.com/api/get?";
+			$key = "key=1132f2389852e19d629e0b7d9d7b784f";
+			$rId = "rId=" . $this->id;
+
+			$response = file_get_contents($endpoint . $key . "&" . $rId);
+		}
+
 		return $response;
 	}
 }
--- include/actions/externalSearch.php
+++ include/actions/externalSearch.php
@@ -1,4 +1,5 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../include/modules/Unirest/Unirest.php';
 
 class ExternalSearch extends Action {
 	public $query = "";
@@ -10,11 +11,34 @@ class ExternalSearch extends Action {
 	}
 
 	public function process() {
-		$endpoint = "https://food2fork.com/api/search?";
-		$key = "key=1132f2389852e19d629e0b7d9d7b784f";
-		$search = "q=" . $this->query;
+		$use = 3;
+		if ($use == 1) {
+			$response = Unirest\Request::get("https://food2fork.com/api/search", null,
+				array(
+					"key" => "1132f2389852e19d629e0b7d9d7b784f",
+					"q" => $this->query
+				)
+			);
+		} elseif ($use == 2) {
+			$response = Unirest\Request::get("https://api.edamam.com/search", null,
+				array(
+					"app_id" => "19ffa89f",
+					"app_key" => "e85df3f5fe982b53d140271c94193b40",
+					"q" => $this->query
+				)
+			);
+		} elseif ($use == 3) {
+			$response = Unirest\Request::get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search",
+				array(
+					"X-Mashape-Key" => "szCweNvVtTmshzgBOVW0iilv2Brtp1XPFwijsnTw76PcD3MKwf",
+					"Accept" => "application/json"
+				),
+				array(
+					"query" => $this->query
+				)
+			);
+		}
 
-		$response = file_get_contents($endpoint . $key . "&" . $search);
 		return $response;
 	}
 }
