6b4b629546be24518697b373e4ab11b6f5511b4e
katiedeangelis <Maxwell.DeAngelis@gmail.com>
2020-07-28 22:06:23 -0400

    Updated to better format the message with a file and line number that is passed in for now

--- include/classes/logger.php
+++ include/classes/logger.php
@@ -10,32 +10,49 @@ class LogLevel {
 class Logger {
   static $logLevel = LogLevel::INFO;
 
+  private static function printLog($prefix, $file, $msg, $line) {
+    $trace = debug_backtrace(true);
+    $caller = $trace[2];
+
+    // TODO: File name does not work for classes since they are not being called
+    // Break on the / for files
+    // $path = explode("/", $caller['file']);
+    // $path = end($path);
+    // Break on . for extension
+    // $file = explode(".", $path)[0];
+
+    // Normalize to length of CRITICAL 
+    $prefix = str_pad("[{$prefix}]", 10, " ");
+    
+    error_log("{$prefix} ({$file}->{$caller['function']}, line {$line}) - {$msg}");
+  }
+
   public static function setLogLevel($level) {
     self::$logLevel = $level;
   }
 
-  public static function critical($message) {
-    error_log("[CRITICAL] {$message}");
+  public static function critical($file, $message, $line) {
+    self::printLog("CRITICAL", $file, $message, $line);
 	}
 
-  public static function error($message) {
+  public static function error($file, $message, $line) {
     if (self::$logLevel < 1) return;
-    error_log("[ERROR] {$message}");
+    self::printLog("ERROR", $file, $message, $line);
 	}
 
-  public static function warning($message) {
+  public static function warning($file, $message, $line) {
     if (self::$logLevel < 2) return;
-    error_log("[WARNING] {$message}");
+    self::printLog("WARNING", $file, $message, $line);
 	}
 
-  public static function debug($message) {
+  public static function debug($file, $message, $line) {
     if (self::$logLevel < 3) return;
-    error_log("[DEBUG] {$message}");
+    self::printLog("DEBUG", $file, $message, $line);
 	}
 
-	public static function info($message) {
+	public static function info($file, $message, $line) {
     if (self::$logLevel < 4) return;
-    error_log("[INFO] {$message}");
+    self::printLog("INFO", $file, $message, $line);
 	}
 }
 ?>
