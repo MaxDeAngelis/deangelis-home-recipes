5c8ddd2951c91fe8e991d9f0159ff1797dc5c1f1
katiedeangelis <Maxwell.DeAngelis@gmail.com>
2020-08-05 20:07:18 -0400

    Updated spacing

--- src/Components/Autocomplete/index.js
+++ src/Components/Autocomplete/index.js
@@ -18,7 +18,7 @@ const styles = theme => ({
     paper: {
         position: 'absolute',
         zIndex: 1,
-        marginTop: theme.spacing.unit,
+        marginTop: theme.spacing(1),
         left: 0,
         right: 0,
     },
